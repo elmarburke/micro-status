a4a1f7e8a71baff338e2e0551cfaf9826eab4c1b
Hans-Christian Otto <c.otto@lab9.de>
2017-03-15 19:24:18 +0100

    added yesterdays pouch prototype

--- src/data/status/actions.js
+++ src/data/status/actions.js
@@ -1,23 +1,18 @@
-import sleep from 'then-sleep'
+import PouchDB from 'pouchdb'
 
 export const FETCH = 'data/status/FETCH'
 export const ADD = 'data/status/ADD'
 
+const db = new PouchDB('micro-status');
+
 export const fetchStatus = () => ({
   type: FETCH,
-  payload: sleep(2000).then(() => [
-    // Auch hier kannst du dich austoben. Sollte die PouchDB magie hier zu aufwendig werden,
-    // kannst du eine api.js anlegen und die functions hier her importieren.
-    { _id: 's3', userId: 'u3', text: 'Ipsum lorem', time: '2017-03-13T15:52:00' },
-    { _id: 's4', userId: 'u4', text: 'sit doro atem', time: '2017-03-11T09:32:00' }
-  ])
-})
+  payload: db.allDocs({include_docs: true}).then(result => result.rows.map(row => row.doc))
+});
 
 export const addStatus = (status) => {
   return {
     type: ADD,
-    // Hier kannst du dann in der Payload ein Promise einfügen.
-    // redux-promise macht die magie.
-    payload: sleep(750).then(() => status)
+    payload: db.put(status).then(result => db.get(result.id))
   }
 }
