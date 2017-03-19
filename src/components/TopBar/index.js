e14680117c2bfd90aa299f9631ac1a9fd667cd52
Hans-Christian Otto <c.otto@lab9.de>
2017-03-19 15:09:23 +0100

    Refactored TopBar to be a PureComponent

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import TopBarWrapper from './components/TopBarWrapper'
 import Logo from './components/Logo'
 import HeaderLink from './components/HeaderLink'
 import Navigation from './components/Navigation'
 
-const TopBar = () => (
-  <TopBarWrapper>
-    <Logo />
-    <Navigation>
-      <HeaderLink to='/'>Home</HeaderLink>
-      <HeaderLink to='/info'>Info</HeaderLink>
-    </Navigation>
-  </TopBarWrapper>
-)
+class TopBar extends PureComponent {
+  render() {
+    return (
+      <TopBarWrapper>
+        <Logo />
+        <Navigation>
+          <HeaderLink to='/'>Home</HeaderLink>
+          <HeaderLink to='/info'>Info</HeaderLink>
+        </Navigation>
+      </TopBarWrapper>
+    )
+  }
+}
 
 export default TopBar
